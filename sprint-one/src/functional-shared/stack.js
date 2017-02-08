14768a047a69da469ca19be2f4b2770774c72f88
abianco3 <anthony.bianco3@gmail.com>
2017-02-08 15:37:56 -0800

    Implement stack class using functional shared class style

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.length = 0;
+
+  _.extend(obj, stackMethods);
+
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    var index = this.length;
+    this.storage[index] = value;
+    this.length++;
+  },
+  pop: function() {
+    if (this.length > 0) {
+      this.length--;
+      var index = this.length;
+      var result = this.storage[index];
+      delete this.storage[index];
+      return result;
+    }
+  },
+  size: function() {
+    return this.length;
+  }
+};
 
 
