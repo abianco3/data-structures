870daf98f36dcb1153917b2ec12b0be86256fca2
abianco3 <anthony.bianco3@gmail.com>
2017-02-09 15:51:49 -0800

    Implement tree class with functional shared-methods pattern

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +12,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children[this.children.length] = child;
 };
 
 treeMethods.contains = function(target) {
+  return this._searchNode(this, target);
+};
+
+treeMethods._searchNode = function (subTree, target) {
+  
+  if (subTree.value === target) {
+    return true;
+  }
+
+  for (var i = 0; i < subTree.children.length; i++) {
+    if (subTree._searchNode(subTree.children[i], target)) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
 
