6887d56f5a8fe1f0a12edaf9248b7b544aa62655
abianco3 <anthony.bianco3@gmail.com>
2017-02-10 14:56:44 -0800

    Implement a basic hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,29 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, [k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var res = this._storage.get(index);
+  for (var i = 0; i < res.length; i++) {
+    if (res[i][0] === k) {
+      return res[i][1];
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var res = this._storage.get(index);
+  for ( var i = 0; i < res.length; i++) {
+    if (res[i][0] === k) {
+      res.splice(i, 1);
+    }
+  }
+  this._storage.set(index, [undefined, undefined]);
 };
 
 
